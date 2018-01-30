2716dd6e396e29f70dcecdfb94589f9f58001dce
NISH1001 <nishanpantha@gmail.com>
2018-01-30 16:25:24 +0545

    Refactor code by Karpathy

--- rnn_word_level.py
+++ rnn_word_level.py
@@ -11,19 +11,41 @@ print('data has {} words, {} unique.'.format(data_size, vocab_size))
 word_to_ix = { ch:i for i,ch in enumerate(words) }
 ix_to_word = { i:ch for i,ch in enumerate(words) }
 
+
 # hyperparameters
-hidden_size = 100 # size of hidden layer of neurons
-seq_length = 25 # number of steps to unroll the RNN for
+
+# size of hidden layer of neurons :: n
+hidden_size = 100
+
+# number of steps to unroll the RNN for :: input length :: m
+seq_length = 25
 learning_rate = 1e-1
 
-# model parameters
-Wxh = np.random.randn(hidden_size, vocab_size)*0.01 # input to hidden
-Whh = np.random.randn(hidden_size, hidden_size)*0.01 # hidden to hidden
-Why = np.random.randn(vocab_size, hidden_size)*0.01 # hidden to output
-bh = np.zeros((hidden_size, 1)) # hidden bias
-by = np.zeros((vocab_size, 1)) # output bias
+"""
+    ht = activate(U*xt + W*ht-1 + b)
+    yt = softmax(V*ht)
+"""
 
-def lossFun(inputs, targets, hprev):
+# input to hidden :: U
+Wxh = np.random.randn(hidden_size, vocab_size)*0.01
+
+# hiden to hidden :: W
+Whh = np.random.randn(hidden_size, hidden_size)*0.01
+
+# hidden to output :: V
+Why = np.random.randn(vocab_size, hidden_size)*0.01
+
+# hidden bias
+bh = np.zeros((hidden_size, 1))
+
+# output bias
+by = np.zeros((vocab_size, 1))
+
+def softmax(x):
+    e = np.exp(x - np.amax(x))
+    return e / np.sum(e)
+
+def train(inputs, targets, hprev):
     """
     inputs,targets are both list of integers.
     hprev is Hx1 array of initial hidden state
@@ -34,35 +56,48 @@ def lossFun(inputs, targets, hprev):
     loss = 0
     # forward pass
     for t in range(len(inputs)):
-        xs[t] = np.zeros((vocab_size,1)) # encode in 1-of-k representation
+        # encode in 1-of-k representation
+        xs[t] = np.zeros((vocab_size,1))
         xs[t][inputs[t]] = 1
-        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh) # hidden state
-        ys[t] = np.dot(Why, hs[t]) + by # unnormalized log probabilities for next words
-        ps[t] = np.exp(ys[t]) / np.sum(np.exp(ys[t])) # probabilities for next words
-        loss += -np.log(ps[t][targets[t],0]) # softmax (cross-entropy loss)
+
+        # hidden state => activate(U*xt + W*ht-a + b)
+        hs[t] = np.tanh(np.dot(Wxh, xs[t]) + np.dot(Whh, hs[t-1]) + bh)
+
+        # unnormalized log probabilities for next words
+        ys[t] = np.dot(Why, hs[t]) + by
+        # probabilities for next words -> softmax
+        ps[t] = softmax(ys[t])
+
+        # cross entropy loss
+        loss += -np.log(ps[t][targets[t], 0])
+
     # backward pass: compute gradients going backwards
     dWxh, dWhh, dWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
     dbh, dby = np.zeros_like(bh), np.zeros_like(by)
     dhnext = np.zeros_like(hs[0])
     for t in reversed(range(len(inputs))):
         dy = np.copy(ps[t])
-        dy[targets[t]] -= 1 # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
+        # backprop into y
+        dy[targets[t]] -= 1
         dWhy += np.dot(dy, hs[t].T)
         dby += dy
-        dh = np.dot(Why.T, dy) + dhnext # backprop into h
-        dhraw = (1 - hs[t] * hs[t]) * dh # backprop through tanh nonlinearity
+        # backprop into h
+        dh = np.dot(Why.T, dy) + dhnext
+        # backprop through tanh nonlinearity
+        dhraw = (1 - hs[t] * hs[t]) * dh
         dbh += dhraw
         dWxh += np.dot(dhraw, xs[t].T)
         dWhh += np.dot(dhraw, hs[t-1].T)
         dhnext = np.dot(Whh.T, dhraw)
     for dparam in [dWxh, dWhh, dWhy, dbh, dby]:
-        np.clip(dparam, -5, 5, out=dparam) # clip to mitigate exploding gradients
+        # clip to mitigate exploding gradients
+        np.clip(dparam, -5, 5, out=dparam)
     return loss, dWxh, dWhh, dWhy, dbh, dby, hs[len(inputs)-1]
 
 def sample(h, seed_ix, n):
     """
-    sample a sequence of integers from the model
-    h is memory state, seed_ix is seed letter for first time step
+        sample a sequence of integers from the model
+        h is memory state, seed_ix is seed word for first time step
     """
     x = np.zeros((vocab_size, 1))
     x[seed_ix] = 1
@@ -79,13 +114,20 @@ def sample(h, seed_ix, n):
 
 n, p = 0, 0
 mWxh, mWhh, mWhy = np.zeros_like(Wxh), np.zeros_like(Whh), np.zeros_like(Why)
-mbh, mby = np.zeros_like(bh), np.zeros_like(by) # memory variables for Adagrad
-smooth_loss = -np.log(1.0/vocab_size)*seq_length # loss at iteration 0
+
+# memory variables for Adagrad
+mbh, mby = np.zeros_like(bh), np.zeros_like(by)
+
+# loss at iteration 0
+smooth_loss = -np.log(1.0/vocab_size)*seq_length
+
 while True:
     # prepare inputs (we're sweeping from left to right in steps seq_length long)
     if p+seq_length+1 >= len(data) or n == 0:
-        hprev = np.zeros((hidden_size,1)) # reset RNN memory
-        p = 0 # go from start of data
+        # reset RNN memory
+        hprev = np.zeros((hidden_size, 1))
+        # go from start of data
+        p = 0
     inputs = [word_to_ix[ch] for ch in data[p:p+seq_length]]
     targets = [word_to_ix[ch] for ch in data[p+1:p+seq_length+1]]
 
@@ -96,7 +138,7 @@ while True:
         print('----\n {} \n----'.format(txt))
 
     # forward seq_length wordacters through the net and fetch gradient
-    loss, dWxh, dWhh, dWhy, dbh, dby, hprev = lossFun(inputs, targets, hprev)
+    loss, dWxh, dWhh, dWhy, dbh, dby, hprev = train(inputs, targets, hprev)
     smooth_loss = smooth_loss * 0.999 + loss * 0.001
     if n % 100 == 0:
         print('iter {}, loss: {}'.format(n, smooth_loss)) # print progress
